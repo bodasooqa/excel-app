c77be77847c815da1163e6f992f6bd162c4e51a6
bodasooqa <bodasooqa@gmail.com>
2020-05-18 21:07:56 +0300

    Resize complete, base optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
+import { $ } from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,7 +18,32 @@ export class Table extends ExcelComponent {
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      console.log('resize');
+      event.preventDefault();
+
+      const type = event.target.dataset.resize;
+      const $resizer = $(event.target);
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coords = $parent.getCoords();
+      const id = $parent.data.col;
+      const cells = this.$root.getAll(`[data-col="${id}"]`);
+
+      document.onmousemove = e => {
+        $resizer.$el.focus();
+        const delta = e[type === 'col' ? 'pageX' : 'pageY'] -
+          coords[type === 'col' ? 'right' : 'bottom'];
+        const value = coords[type === 'col' ? 'width' : 'height'] + delta;
+        $parent.$el.style[type === 'col' ? 'width' : 'height'] = value + 'px';
+
+        if (type === 'col') {
+          cells.forEach(item => {
+            item.style.width = value + 'px';
+          })
+        }
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      }
     }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -9,7 +9,7 @@ function createRow(index, content) {
     '';
 
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index || ''}
         ${resize}
@@ -19,9 +19,9 @@ function createRow(index, content) {
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -32,9 +32,9 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index);
 }
 
-function toCell() {
+function toCell(_, index) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${index}"></div>
   `
 }
 
